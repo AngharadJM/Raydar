1f04c5d4260a388504bd0de52094fdb0da5b9074
AngharadJM <ajmw287@gmail.com>
2025-05-13 23:22:06 +0100

    Added the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -55,7 +55,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayforecast() {
+  let days = [`Mon`, `Tues`, `Weds`, `Thurs`, `Fri`];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">      
+      <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>16°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+          </div>
+          `;
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Manchester");
+displayforecast();
